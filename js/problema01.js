74a20c2e9f74a28075e8585334a3f5a76cb8fe31
LeonardoHdz93 <lhernandezm1909@alumno.ipn.mx>
2021-03-24 09:45:00 -0600

    pagina hobbies

--- js/problema01.js
+++ js/problema01.js
@@ -16,18 +16,57 @@ function validarn(e){
 
 
 function interes(){
-   
     var valor = document.formulario.cantidad.value;
+    var valorm = document.formulario.cantidadm.value;
+
+    if(valor > 0, valorm > 0){
     var result = parseInt(valor);
-    var interes = result*0.02;
+    var meses = parseInt(valorm);
+    var interes = result*(meses*0.02);
     var total = result+interes;
+    
 
     document.formulario.sueldoti.value = "$" + total;
-    
+    }else{
+        alert("Completa los campos anteriores")
+    }
     
 }
 
 function borrar1(){
     document.formulario.sueldoti.value = "";
     document.formulario.cantidad.value = "";
+}
+
+function validardos(formulario){
+
+    if(formulario.cantidadm.value <3 > 24){
+        alert("Pon un numero entre 3 y 24 meses");
+        formulario.cantidadm.focus();
+    return false;
+    }
+
+    var checkOk = "3"+"4"+"5"+"6"+"7"+"8"+"9"+"10"+"11"+"12"+"13"+"14"+"15"+"16"+"17"+"18"+"19"+"20"+"21"+"22"+"23"+"24";
+
+    var checkStr = formulario.cantidadm.value;
+
+    var allValid = true;
+
+    for(var i  = 0; i < checkStr.length; i++) {
+        var ch = checkStr.charAt(i);
+        for(var j = 0;  j < checkOk.length; j++)
+        if(ch == checkOk.charAt(j))    
+                break;
+        
+       if(j == checkOk.length){
+           allValid = false;
+           break;
+       } 
+    }
+
+    if(!allValid){
+        alert("Pon un numero entre 3 y 24 meses");
+        formulario.cantidadm.focus();
+        return false;
+    }
 }
